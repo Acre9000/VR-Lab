fc508ef621c8ba040bd750c5f5bf5eb7a8973c00
Acre9000 <45312776+Acre9000@users.noreply.github.com>
2022-09-01 16:15:34 +0200

    Things Work!

--- Assets/MovePhysics.cs
+++ Assets/MovePhysics.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class MovePhysics : MonoBehaviour
 {
+
+    public Transform target;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+
+        Quaternion rotDiff = target.rotation * Quaternion.Inverse(transform.rotation);
+        rotDiff.ToAngleAxis(out float angleDegree, out Vector3 rotAxis);
+
+        Vector3 rotDiffDegree = angleDegree * rotAxis;
+
+        rb.angularVelocity = (rotDiffDegree * Mathf.Deg2Rad / Time.fixedDeltaTime);
     }
+
+
 }
